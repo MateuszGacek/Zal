1330d6ebda1f3ccbbc353dc1ad206ee7783e0ce3
MateuszGacek <mateusz_gacek@interia.pl>
2020-02-29 14:14:40 +0100

    zal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,5 +15,7 @@ public class Main {
 	System.out.println("zaliczenie");
 	System.out.println("zaliczenie");
 	System.out.println("zaliczenie");
+	System.out.println("zaliczenie");
+	System.out.println("zaliczenie");
     }
 }
